812f54d5d6a216e3db290c81f865329231e3daa8
NikoRye <nibo1001@stud.ek.dk>
2026-06-08 15:44:55 +0200

    Add scroll progress bar and update image dimensions in temaer.css for improved layout

--- script.js
+++ script.js
@@ -81,3 +81,13 @@ if (popup && popupImg) {
     }
   });
 }
+
+/* scroll bar */
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+  const progress = (scrollTop / docHeight) * 100;
+
+  document.querySelector(".progress-bar").style.width = progress + "%";
+});
